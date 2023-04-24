11d9c7ed0f627b30abd7b32bf4a1a03e8bd138be
Agrawalyogu <131667309+Agrawalyogu@users.noreply.github.com>
2023-04-24 17:40:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit;
 
-streamlit.title("Snowflake")
-
-streamlit.header("yogita agrawal")
-
-streamlit.text("Data Engineer")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🐔Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑Hard-Boiled Free-Range Egg')
